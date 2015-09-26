e822e85b77ae5a0a391d41f38b08a79e793a7645
LyleAlberto <lylearomero@gmail.com>
2015-09-26 11:38:45 -0500

    matriz completa sin error

--- codigo_2.m
+++ codigo_2.m
@@ -5,13 +5,15 @@ disp('Ruht Horwihts')
 d=input('ingresar el polinomio entre []');
 poly2sym(d)%dibuja el polinomio
 N=length(d);%numero de elementos del vector
-mo=mod(N-1,2);
+mo=mod(N,2);
 x1(1,:)=d(1:2:N);
 if mo~=0
     N1=N+1;
     d(N1)=0;
+    x1(2,:)=d(2:2:N1);
+else 
+    x1(2,:)=d(2:2:N);
 end
-x1(2,:)=d(2:2:N1);
 lim=round(N/2)
     for i=3:1:N%fila
         n=1;
